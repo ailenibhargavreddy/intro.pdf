d9c7e0466370499f78cd18a438f4960fa121f793
ailenibhargavreddy <147069034+ailenibhargavreddy@users.noreply.github.com>
2023-10-05 22:00:56 +0530

    Delete chapter .py

--- chapter .py
+++
@@ -1,9 +0,0 @@
-n = 3
-for i in range(1,n):
-    for j in range(n-i):
-        print(" ",end="")
-    for j in range(2*i-1):
-        print("*",end="")
-    for j in range(n-i):
-        print(" ",end="")
-    print("\n",end="")
